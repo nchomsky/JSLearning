46131cf955c7ed88c33aad463d4a6358e59cb771
Noah Chomsky <nb.chomsky@gmail.com>
2021-12-20 23:30:01 -0500

    DOM Push

---
+++ Section 5 - DOM Manipulation/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+console.log(document.querySelector('.message').textContent);
