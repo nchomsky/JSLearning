760af0ee9ebf320b087767bcde8850003820a672
Noah Chomsky <nbchomp03@gmail.com>
2021-12-22 00:03:59 -0500

    Refactor so it's more DRY

--- Section 6 - Modal/script.js
+++ Section 6 - Modal/script.js
@@ -3,4 +3,27 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('.show-modal');
+
+// creates a nodeList of the elements
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', () => {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', () => {
+    closeModal();
+})
+
+overlay.addEventListener('click', () => {
+    closeModal();
+})
