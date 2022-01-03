a094228eb2cca2d6ff075450229e4c62f64090d7
Noah Chomsky <nbchomp03@gmail.com>
2022-01-03 01:05:01 -0500

    Hoisting Refresher

--- Section 8 - Behind the Scenes/script.js
+++ Section 8 - Behind the Scenes/script.js
@@ -26,3 +26,41 @@ const firstName = 'Noah';
 calcAge(1991);
 // Below will not work because functions are block scoped... the global Execution context does not have access to PrintAge
 // printAge();
+
+
+
+//----Hoisting Refresher----\\
+
+
+// below will show undefined
+console.log(me);
+
+// below will return error saying can't access before intiliziation
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas'
+let job = 'teacher'
+const year = 1991;
+
+// functions
+console.log(addDecl(2,3));
+console.log(addExpr(2,3));
+console.log(addArrow(2,3));
+
+function addDecl(a,b){
+    return a + b;
+}
+
+const addExpr = function (a,b){
+    return a+b
+}
+
+const addArrow = (a,b) => a+b;
+
+
+var numProducts = 10;
+
+function deleteShoppingCart(){
+    console.log('All products deleted!');
+}
