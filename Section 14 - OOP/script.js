4b43fae74b3c37aba3aad8bd47e5d3b514a4be83
Noah Chomsky <nbchomp03@gmail.com>
2022-01-15 02:37:06 -0500

    added function contructors

--- Section 14 - OOP/script.js
+++ Section 14 - OOP/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this
+    // this.calcAge = function(){
+    //     console.log(2037-this.birthYear);
+    // }
+}
+
+const jonas = new Person('Jonas', 1996);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 2000);
